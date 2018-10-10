a96762319356119bb4e0c107fc9ba1146c04d423
Michael Hansen <mihansen@microsoft.com>
2018-10-10 07:25:54 -0700

    Refactored into library and executable

--- src/FhirExport/Program.cs
+++ src/FhirExport/Program.cs
@@ -4,9 +4,6 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
-using Microsoft.IdentityModel.Clients.ActiveDirectory;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using FhirExport;
 
 namespace FhirExport
@@ -14,11 +11,9 @@ namespace FhirExport
     class Program
     {
         public static IConfiguration Configuration { get; set; }
-        private static AuthenticationContext AuthContext { get; set; }
-        private static ClientCredential ClientCredential { get; set; }
-        private static String Audience { get; set; }
         private static String OutputFile { get; set; }
 
+        private static FhirAuthenticator FhirAuth { get; set;}
         static void Main(string[] args)
         {
             Task.Run(() => MainAsync(args)).Wait();
@@ -47,65 +42,15 @@ namespace FhirExport
             Console.WriteLine($"Azure AD Audience       : {Configuration["AzureAD_Audience"]}");
             Console.WriteLine($"Output File             : {OutputFile}");
 
-            AuthContext = new AuthenticationContext(Configuration["AzureAD_Authority"]);
-            ClientCredential = new ClientCredential(Configuration["AzureAD_ClientId"], Configuration["AzureAD_ClientSecret"]); ;
-            Audience = Configuration["AzureAD_Audience"];
+            FhirAuth = new FhirAuthenticator(Configuration["AzureAD_Authority"], 
+                                             Configuration["AzureAD_ClientId"], 
+                                             Configuration["AzureAD_ClientSecret"], 
+                                             Configuration["AzureAD_Audience"]);
 
             string query = Configuration["FhirQuery"];
             while (!String.IsNullOrEmpty(query)) 
             {
-                query = await AppendQueryToFile(Configuration["FhirServerUrl"], query, OutputFile);
-            }
-        }
-
-        private static AuthenticationResult GetAuthenticationResult()
-        {
-            return AuthContext.AcquireTokenAsync(Audience, ClientCredential).Result;
-        }
-
-        private static async Task<string> AppendQueryToFile(string serverUrl, string query, string fileName)
-        {
-            long records = 0;
-
-            using (var client = new HttpClient())
-            {
-                client.BaseAddress = new Uri(serverUrl);
-                client.DefaultRequestHeaders.Add("Authorization", "Bearer " + GetAuthenticationResult().AccessToken);
-
-                HttpResponseMessage getResult = getResult = await client.GetAsync(query);
-
-                JObject bundle = JObject.Parse(await getResult.Content.ReadAsStringAsync());
-                JArray entries = (JArray)bundle["entry"];
-
-                for (int i = 0; i < entries.Count; i++)
-                {
-                    string entry_json = (((JObject)entries[i])["resource"]).ToString(Formatting.None);
-                    AppendToFile(fileName, entry_json);
-                    records++;
-                }
-
-                JArray links = (JArray)bundle["link"];
-                for (int i = 0; i < links.Count; i++)
-                {
-                    string link_type = (string)(bundle["link"][i]["relation"]);
-                    string link_url = (string)(bundle["link"][i]["url"]);
-
-                    if (link_type == "next")
-                    {
-                        Uri nextUri = new Uri(link_url);
-                        return nextUri.PathAndQuery;
-                    }
-                }
-
-                return "";
-            }
-        }
-
-        private static void AppendToFile(string fileName, string appendString)
-        {
-            using (StreamWriter w = File.AppendText(fileName))
-            {
-                w.WriteLine(appendString);
+                query = await FhirQuery.AppendQueryToFile(Configuration["FhirServerUrl"], query, OutputFile, FhirAuth);
             }
         }
     }
--- src/FhirExportLib/FhirExport.cs
+++ src/FhirExportLib/FhirExport.cs
@@ -1,8 +1,88 @@
 ﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
-namespace FhirExportLib
+namespace FhirExport
 {
-    public class Class1
+    public class FhirAuthenticator
     {
+        
+        private static AuthenticationContext AuthContext { get; set; }
+        private static ClientCredential ClientCredential { get; set; }
+        private static String Audience { get; set; }
+
+        public FhirAuthenticator(string authority,
+                                string clientId,
+                                string clientSecret,
+                                string audience)
+        {
+            AuthContext = new AuthenticationContext(authority);
+            ClientCredential = new ClientCredential(clientId, clientSecret);
+            Audience = audience;
+        }
+
+        public AuthenticationResult GetAuthenticationResult()
+        {
+            return AuthContext.AcquireTokenAsync(Audience, ClientCredential).Result;
+        }
+    }
+
+    public class FhirQuery
+    {
+        public static async Task<string> AppendQueryToFile(string serverUrl, string query, string fileName, FhirAuthenticator fhirAuth)
+        {
+            long records = 0;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(serverUrl);
+                client.DefaultRequestHeaders.Add("Authorization", "Bearer " + fhirAuth.GetAuthenticationResult().AccessToken);
+
+                HttpResponseMessage getResult = getResult = await client.GetAsync(query);
+
+                JObject bundle = JObject.Parse(await getResult.Content.ReadAsStringAsync());
+                JArray entries = (JArray)bundle["entry"];
+
+                for (int i = 0; i < entries.Count; i++)
+                {
+                    string entry_json = (((JObject)entries[i])["resource"]).ToString(Formatting.None);
+                    AppendToFile(fileName, entry_json);
+                    records++;
+                }
+
+                JArray links = (JArray)bundle["link"];
+                for (int i = 0; i < links.Count; i++)
+                {
+                    string link_type = (string)(bundle["link"][i]["relation"]);
+                    string link_url = (string)(bundle["link"][i]["url"]);
+
+                    if (link_type == "next")
+                    {
+                        Uri nextUri = new Uri(link_url);
+                        return nextUri.PathAndQuery;
+                    }
+                }
+
+                return "";
+            }
+        }
+
+        private static void AppendToFile(string fileName, string appendString)
+        {
+            using (StreamWriter w = File.AppendText(fileName))
+            {
+                w.WriteLine(appendString);
+            }
+        }
+    }
+
+    public class BlobUploader
+    {
+
     }
 }
